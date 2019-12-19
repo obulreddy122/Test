9ae38b85500d4d2bc69693537096e3cc88a6c2eb
rama obula reddy <obulreddy122@gmail.com>
2019-12-19 19:06:22 +0530

    DXCWorld.java

--- DXCWorld.java
+++ DXCWorld.java
@@ -3,7 +3,7 @@ public class DXCWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("DXC, World");
+        System.out.println("Bangalore,Hell=World");
     }
 
 }
