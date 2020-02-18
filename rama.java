41bb81c0dc16a0c4194e95ebe8cbfdd77548bae2
obulreddy <obulreddy122@gmail.com>
2020-02-18 20:24:47 +0530

    new file:   bangalore 	new file:   obulreddy 	new file:   obulreddy02142020 	modified:   rama.java

--- rama.java
+++ rama.java
@@ -1,10 +1,7 @@
-public class HelloWorld {
-
-    public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
-    }
-
-}
-
-
+class rama
+{  
+    public static void main(String args[]) 
+    { 
+        System.out.println("Hello, Kadapa"); 
+    } 
+} 
